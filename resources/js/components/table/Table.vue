3c6ef7e8554a1ae6960dbaa612dd33696dce9047
Cleiton Carvalho <cleiton_601@hotmail.com>
2026-05-09 15:45:27 -0300

    feat(Component) Criado componente tabela.

---
+++ resources/js/components/table/Table.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const props = defineProps({
+  icon: String,
+  title: String,
+})
+</script>
+
+<template>
+
+  <div class="mt-5 card border border-base-content/20">
+    <div class="card-title text-neutral p-3 flex items-center gap-1">
+      <FontAwesomeIcon :icon="['fas', icon]" />
+      {{ title }}
+    </div>
+
+    <div class="overflow-x-auto border-t border-base-content/20">
+
+      <table class="table">
+
+        <thead>
+          <slot name="thead"></slot>
+        </thead>
+
+        <tbody>
+          <slot name="tbody"></slot>
+        </tbody>
+
+      </table>
+    </div>
+  </div>
+</template>
