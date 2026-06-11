16348fcdd7997bdac734313f62260bdafe78b739
Cleiton <cleiton_601@hotmail.com>
2026-06-11 15:16:16 -0300

    feat(Diretorio) mudança no diretorio.

--- app/Http/Controllers/Web/Proposta/PropostaController.php
+++ app/Http/Controllers/Web/Proposta/PropostaController.php
@@ -5,15 +5,12 @@ namespace App\Http\Controllers\Web\Proposta;
 use App\Enum\StatusProposta;
 use App\Http\Controllers\Controller;
 use App\Queries\OrigemQuery;
-use App\Services\PropostaService;
+use App\Services\Proposta\PropostaService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PropostaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(
         PropostaService $propostaService,
         OrigemQuery $origemQuery,
--- app/Services/Proposta/PropostaService.php
+++ app/Services/Proposta/PropostaService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Proposta;
 
 use App\Queries\PropostaQuery;
 
