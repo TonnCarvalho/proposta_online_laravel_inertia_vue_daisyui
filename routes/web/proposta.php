4fd4ba00a8c02cf22d56b1519d1a60b7528c0d1a
Cleiton <cleiton_601@hotmail.com>
2026-07-16 16:56:20 -0300

    feat(Route): ajuste rosta de pesquisaCpfCadastro, rota para cadastra associado e nomeclatura do post para store

--- routes/web/proposta.php
+++ routes/web/proposta.php
@@ -1,24 +1,34 @@
 <?php
 
+use App\Http\Controllers\Web\Proposta\PesquisaCpfCadastroController;
 use App\Http\Controllers\Web\Proposta\PropostaController;
 use App\Http\Controllers\Web\Proposta\PropostaCreateController;
 use App\Http\Controllers\Web\Proposta\PropostaStoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/proposta', [PropostaController::class, 'index'])->name('proposta.index');
 
     Route::prefix('/proposta')->group(function () {
-        Route::get('/criar', [PropostaCreateController::class, 'create'])
+        Route::get('/', [PropostaController::class, 'index'])
+            ->name('proposta.index');
+
+        Route::get('/criar/{tipoCadastro?}/{associado?}', [PropostaCreateController::class, 'create'])
+            ->where('tipoCadastro', '[a-z_]+')
+            ->whereNumber('associado')
             ->name('proposta.create');
 
         Route::post('/criar', [PropostaStoreController::class, 'store'])
-            ->name('proposta.post');
+            ->name('proposta.store');
 
         Route::get('/sucesso', [PropostaCreateController::class, 'sucess']);
 
         Route::get('/{id_proposta}/edit', [PropostaController::class, 'edit'])
             ->whereNumber('id_proposta')
             ->name('proposta.edit');
+
+        Route::get('/pesquisa', [PesquisaCpfCadastroController::class, 'index'])
+            ->name('pesquisaCpfCadastro.index');
+
+        Route::get('/pesquisar', [PesquisaCpfCadastroController::class, 'pesquisarAssociado']);
     });
 });
