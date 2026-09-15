b03b88215852f8886148276709ae2d7d18c3451c
Cleiton <cleiton_601@hotmail.com>
2026-09-15 15:36:08 -0300

    feat(Acompanhamento): removido  relacionamento ultimoAcompanhamento

--- app/Models/Proposta.php
+++ app/Models/Proposta.php
@@ -37,19 +37,10 @@ class Proposta extends Model
 
     public $timestamps = false;
 
-    public function acompanhamento(): HasMany
+    public function acompanhamento(): HasOne
     {
-        return $this->hasMany(Acompanhamento::class, 'id_proposta');
+        return $this->hasOne(Acompanhamento::class, 'id_proposta');
     }
-
-public function ultimoAcompanhamento()
-{
-    return $this->hasOne(
-        Acompanhamento::class,
-        'id_proposta',
-        'id_proposta'
-    )->latestOfMany('data_status');
-}
 
     public function usuario(): BelongsTo
     {
