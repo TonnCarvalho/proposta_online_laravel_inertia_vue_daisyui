dc418efc843df4ece344b69e144a89d3b4918c22
Cleiton <cleiton_601@hotmail.com>
2026-09-18 16:45:32 -0300

    feat(StatusProposta) Mudado as cores para tailwindcss e icones para Lucide

--- resources/js/components/StatusProposta.vue
+++ resources/js/components/StatusProposta.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { BadgeCheck, DollarSign, FileCheck, FileCheckCorner, FileClock, FileExclamationPoint, FileMinus, FilePenLine, FileText, FileX, Loader } from '@lucide/vue';
 import { computed } from 'vue';
 
 const props = defineProps({
@@ -13,81 +14,58 @@ const props = defineProps({
 const statusMap = {
     0: {
         label: 'Recusada',
-        background: '#FEE2E2',
-        border: '#FEE2E2',
-        color: '#991B1B',
-        icon: 'circle-xmark',
+        class: 'bg-red-100 text-red-600 border-red-100',
+        icon: FileX,
     },
     1: {
         label: 'Em andamento',
-        class: 'peer-checked:bg-blue-600 peer-checked:text-white peer-checked:border-blue-600',
-        background: '#DBEAFE',
-        border: '#DBEAFE',
-        color: '#1E40AF',
-        icon: 'spinner',
+        class: 'bg-blue-100 text-blue-600 border-blue-100',
+        icon: Loader,
     },
     2: {
         label: 'Em analise',
-        background: '#F3E8FF',
-        border: '#F3E8FF',
-        color: '#8613e4',
-        icon: 'file-lines',
+        class: 'bg-purple-100 text-purple-600 border-purple-100',
+        icon: FileClock,
     },
     3: {
         label: 'Pendente',
-        background: '#FEF3C7',
-        border: '#FEF3C7',
-        color: '#da5e13',
-        icon: 'circle-exclamation',
+        class: 'bg-yellow-100 text-yellow-500 border-yellow-100',
+        icon: FileExclamationPoint,
     },
     4: {
         label: 'Pendencia resolvida',
-        background: '#FFEDD5',
-        border: '#FFEDD5',
-        color: '#9A3412',
-        icon: 'circle-check',
+        class: 'bg-cyan-100 text-cyan-500 border-cyan-100',
+        icon: FileMinus,
     },
     5: {
         label: 'Conferida',
-        background: '#CCFBF1',
-        border: '#CCFBF1',
-        color: '#115E59',
-        icon: 'clipboard-check',
+        class: 'bg-green-100 text-green-500 border-green-100',
+        icon: FileCheck,
     },
     6: {
-        label: 'Assinatura contrato',
-        background: '#E0E7FF',
-        border: '#E0E7FF',
-        color: '#3730A3',
-        icon: 'file-signature',
+        label: 'Aguardando assinatura',
+        class: 'bg-sky-100 text-sky-600 border-sky-100',
+        icon: FilePenLine,
     },
     7: {
         label: 'Contrato assinado',
-        background: '#E0E7FF',
-        border: '#E0E7FF',
-        color: '#3730A3',
-        icon: 'check',
+        class: 'bg-emerald-100 text-emerald-600 border-emerald-100',
+        icon: FileCheckCorner,
     },
     8: {
         label: 'Assinatura CCB',
-        background: '#e7e7e7',
-        border: '#e7e7e7',
-        color: '#454545',
-        icon: 'file-signature',
+        class: 'bg-gray-100 text-gray-600 border-gray-100',
+        icon: FileText,
     },
     9: {
         label: 'Aguardando pagamento',
-        background: '#e5ff95',
-        border: '#e5ff95',
-        color: '#454545',
-        icon: 'dollar-sign',
+        class: 'bg-indigo-100 text-indigo-500 border-indigo-100',
+        icon: DollarSign,
     },
     10: {
         label: 'Pago',
-        background: '#D1FAE5',
-        border: '#D1FAE5',
-        color: '#065F46',
-        icon: 'dollar-sign',
+        class: 'bg-green-100 text-green-700 border-green-100',
+        icon: BadgeCheck,
     },
 }
 
@@ -104,13 +82,11 @@ const statusConfig = computed(() => {
 
 <template>
     <span class="badge whitespace-nowrap"
-        :style="{
-            backgroundColor: statusConfig.background,
-            color: statusConfig.color,
-            borderColor: statusConfig.border
-        }">
-        <FontAwesomeIcon :icon="statusConfig.icon"
-            class="text-xs" />
+        :class="statusConfig.class">
+
+        <component :is="statusConfig.icon"
+            class="size-4" />
+
         {{ statusConfig.label }}
     </span>
 </template>
