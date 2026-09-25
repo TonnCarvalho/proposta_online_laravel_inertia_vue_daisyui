e6a4c47be5eea0edd2223cda91c9029b099c3bcc
Cleiton <cleiton_601@hotmail.com>
2026-09-25 16:52:55 -0300

    feat: mudança no modal | melhorias na visualização do documento da proposta

--- resources/js/components/modal/Modal.vue
+++ resources/js/components/modal/Modal.vue
@@ -1,18 +1,45 @@
 <script setup>
+import { ref } from 'vue';
 
-const props = defineProps({
-    titulo: String,
-    id: String,
+const dialog = ref(null)
+
+function showModal() {
+    dialog.value.showModal()
+}
+
+function closeModal() {
+    dialog.value.close()
+}
+
+defineExpose({ showModal, closeModal })
+
+defineProps({
+    tamanho: {
+        type: String,
+        default: 'min-w-1',
+    },
 })
+
 </script>
 <template>
-    <dialog :id="id"
+    <dialog ref="dialog"
         class="modal">
-        <div class="modal-box min-w-4/5">
-            <h3 v-if="titulo" class="text-lg font-bold">
-                {{ titulo }}
-            </h3>   
-            <slot />
+        <div class="modal-box" :class="tamanho">
+            <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                    x
+                </button>
+            </form>
+
+            <h3 class="text-lg font-bold mb-3">
+                <slot name="header"></slot>
+            </h3>
+            
+            <slot name="content"></slot>
+
+            <div class="modal-action">
+                <slot name="action"></slot>
+            </div>
         </div>
 
         <form method="dialog"
--- resources/js/pages/proposta/editar/DocumentosView.vue
+++ resources/js/pages/proposta/editar/DocumentosView.vue
@@ -2,7 +2,6 @@
 import Card from '@/components/card/Card.vue';
 import CardBody from '@/components/card/CardBody.vue';
 import CardTitle from '@/components/card/CardTitle.vue';
-import Modal from '@/components/modal/Modal.vue';
 import { Download, Trash } from '@lucide/vue';
 import { ref } from 'vue';
 
@@ -14,11 +13,15 @@ const props = defineProps({
     imagemVersao: String,
 })
 
+
+const dialog = ref(null);
+const itemModal = ref(null)
 const documentoSelecionado = ref(null);
 
-function visualizarDocumento(item) {
-    documentoSelecionado.value = item;
-    document.getElementById('ver_documento').showModal();
+function openModal(item) {
+    itemModal.value = item
+    documentoSelecionado.value = item.url
+    dialog.value.showModal();
 }
 
 </script>
@@ -26,7 +29,8 @@ function visualizarDocumento(item) {
 
     <Card class="mb-3">
         <CardBody>
-            <CardTitle title="Documentos" icon="FileUser" />
+            <CardTitle title="Documentos"
+                icon="FileUser" />
             <div class="grid grid-cols-1 lg:grid-cols-5 gap-3">
                 <div v-for="item in documentos"
                     :key="item.titulo">
@@ -36,9 +40,8 @@ function visualizarDocumento(item) {
                             <div class="font-bold">
                                 {{ item.titulo }}
                             </div>
-
                             <button class="cursor-pointer w-full"
-                                @click="visualizarDocumento(item)">
+                                @click="openModal(item)">
 
                                 <img v-if="item.tipo === 'imagem'"
                                     class="w-full min-h-32 max-h-32 mx-auto object-contain"
@@ -71,12 +74,35 @@ function visualizarDocumento(item) {
         </CardBody>
     </Card>
 
-    <Modal id="ver_documento"
-        :titulo="documentoSelecionado?.titulo">
+    <dialog ref="dialog"
+        class="modal">
+        <div class="modal-box min-w-5xl">
+            <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                    x
+                </button>
+            </form>
 
-        <iframe :src="documentoSelecionado?.url"
-            class="w-full h-[85vh]">
-        </iframe>
-    </Modal>
+            <h3 class="text-lg font-bold mb-3">
+                {{ itemModal?.titulo }}
+            </h3>
 
+            <div class="modal-action">
+
+                <iframe v-if="itemModal?.tipo == 'pdf'"
+                    :src="documentoSelecionado"
+                    class="w-full h-[84vh]">
+                </iframe>
+
+                <img v-if="itemModal?.tipo != 'pdf'"
+                    :src="documentoSelecionado"
+                    class="w-full h-[84vh] object-contain" />
+            </div>
+        </div>
+
+        <form method="dialog"
+            class="modal-backdrop">
+            <button>close</button>
+        </form>
+    </dialog>
 </template>
